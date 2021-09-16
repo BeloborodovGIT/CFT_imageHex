54ebca01d9fabebbd303fde8802b37f4513786d6
BeloborodovGIT <73323084+BeloborodovGIT@users.noreply.github.com>
2021-09-16 15:58:57 +0700

    Initial commit

---
+++ img/index/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .forms import ImageForm
+from PIL import Image
+
+
+def color_count(f):
+    img = Image.open(f)
+    black = 0
+    white = 0
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    pix = list(img.getdata())
+    hex_pix = [hex((r << 16) + (g << 8) + b) for r, g, b in pix]
+    for p in hex_pix:
+        if p == '0x0':
+            black += 1
+        elif p == '0xffffff':
+            white += 1
+    return white, black
+
+
+def index(request):
+    context = {}
+    
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            colors = color_count(request.FILES['image'])
+            context['image'] = request.FILES['image']
+            context['white'] = colors[0]
+            context['black'] = colors[1]
+        else:
+            form = ImageForm()
+        context['form'] = form
+
+    return render(request=request, template_name='index/index.html', context=context)
